4a928a7560b01b0b123bf9cba49364643c44c20a
fallen-icarus <modern.daidalos+git@gmail.com>
2023-02-16 13:19:57 -0500

    Initial setup

---
+++ src/CardanoDatums.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE NumericUnderscores    #-}
+{-# LANGUAGE BangPatterns          #-}
+
+module CardanoDatums
+(
+
+) where
